092a603440c98acf5d295d7a74eccad5b64c289c
dldhk97 <dldhk97@gmail.com>
2020-09-07 00:38:45 +0900

    간단한 CLI 코드 추가해서 하나의 게시글만 크롤링 할 수 있게 하였음.

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ from datetime import datetime
 # 'UTF-8'이 아닌, 'MS949'로 할 경우 엑셀에서 바로 열 수 있지만, 특정 문자(\u2027)가 포함된 경우 오류가 납니다.
 CSV_ENCODING_TYPE = 'utf-8'
 
-def parse_and_save(search_word, max_count=None):
+def crawl_by_search_word(search_word, max_count=None):
     blog_post_list = naverblogcrawler.naver_blog_crawling(search_word, 100, "sim", max_count)
 
     if blog_post_list:
         print("텍스트 파일로 저장합니다")
         save_as_csv(search_word, blog_post_list)
-    return
+
+def crawl_single_post(url):
+    blog_post_list = []
+
+    blog_post = naverblogcrawler.pasre_blog_post(url)
+
+    if blog_post:
+        blog_post_list.append(blog_post)
+    
+    if blog_post_list:
+        file_name_header = blog_post._blog_id + ' ' + blog_post._log_no
+
+        print("텍스트 파일로 저장합니다")
+        save_as_csv(file_name_header, blog_post_list)
+
 
 def create_directory(path):
     try:
@@ -23,11 +37,11 @@ def create_directory(path):
             print("Failed to create directory!!!!!")
             raise
 
-def save_as_csv(search_word, blog_post_list):
+def save_as_csv(file_name_header, blog_post_list):
     now = datetime.today().strftime("%Y%m%d%H%M%S")
     save_path = os.getcwd() + '\\crawl\\'
     create_directory(save_path)
-    file_name = now + '-' + search_word + '.csv'
+    file_name = now + '-' + file_name_header + '.csv'
 
     try:
         f = open(save_path + file_name, 'w', encoding=CSV_ENCODING_TYPE)
@@ -51,9 +65,52 @@ def save_as_csv(search_word, blog_post_list):
         print(e)
 
     return
-    
+
+def task_crawl_by_search_word():
+    print('검색어 : ')
+    search_word = input()
+    if search_word is not None:
+        print('크롤링할 포스트의 개수 : ')
+        blog_post_count = input()
+
+        if blog_post_count.isdigit:
+            crawl_by_search_word(search_word, blog_post_count)
+        else:
+            print('크롤링할 포스트의 개수가 올바르지 않습니다.')
+        crawl_by_search_word()
+    else:
+        print('검색어가 올바르지 않습니다.')
+
+def task_crawl_single_post():
+    print('URL : ')
+    url = input()
+    if url is not None:
+        crawl_single_post(url)
+    else:
+        print('URL이 올바르지 않습니다.')
+
+
+
+def simple_cli():
+    while True:
+        try:
+            print('1. 검색어로 크롤링')
+            print('2. URL로 하나의 게시글 크롤링')
+            print('3. 종료')
+            user_input = input()
+
+            if user_input is '1':
+                task_crawl_by_search_word()
+            elif user_input is '2':
+                task_crawl_single_post()
+            elif user_input is '3':
+                break
+
+        except Exception as e:
+            print(e)
+
+    print('종료합니다')
+
 
 if __name__ == '__main__':
-    parse_and_save("대구희망지원금 신청", 10)
-    # parse_and_save("다운로드", 80)
-    # parse_and_save("립버전", 80)
+    simple_cli()
